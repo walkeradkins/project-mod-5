210ca28eacf9e959b52c9ff08503571b82a6d8d9
Walker Adkins <walkeradkins@gmail.com>
2022-05-26 15:55:03 -0500

    api/test removed

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -6,8 +6,9 @@ router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 
-router.post('/test', (req, res) => {
-  res.json({ requestBody: req.body });
-});
+// // test api connection to front end
+// router.post('/test', (req, res) => {
+//   res.json({ requestBody: req.body });
+// });
 
 module.exports = router;
