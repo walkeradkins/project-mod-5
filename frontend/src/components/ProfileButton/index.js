04f696a231751fa8ade95a540213d72fb40d1ff1
Walker Adkins <walkeradkins@gmail.com>
2022-05-26 19:02:45 -0500

    user dropdown basic

--- frontend/src/components/ProfileButton/index.js
+++ frontend/src/components/ProfileButton/index.js
@@ -1,8 +1,49 @@
 import './ProfileButton.css';
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import * as sessionActions from '../../store/session';
 
-const ProfileButton = () => {
+const ProfileButton = ({ user }) => {
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false)
+
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  }
+
+  useEffect(() => {
+    if (!showMenu) return;
+    const closeMenu = () => {
+      setShowMenu(false);
+    };
+
+    document.addEventListener('click', closeMenu);
+    return () => document.removeEventListener('click', closeMenu)
+  }, [showMenu]);
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  }
+
   return (
-    <div><i class="fa-solid fa-user"></i></div>
+    <>
+      <button>
+        <i
+          onClick={openMenu}
+          class="fa-solid fa-user"
+        >
+        </i>
+      </button>
+      {showMenu && (
+        <ul className='profile-dropdown'>
+          <li key={`username`}>{user.username}</li>
+          <li key={`email`}>{user.email}</li>
+          <li key={`logout`}><button onClick={logout}>Log Out</button></li>
+        </ul>
+      )}
+    </>
   )
 }
 
