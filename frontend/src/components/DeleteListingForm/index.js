3c5fd36f98030faa54d9b5c484ad3de4a2b71cf2
Walker Adkins <walkeradkins@gmail.com>
2022-05-31 15:44:57 -0500

    delete listings completed

--- frontend/src/components/DeleteListingForm/index.js
+++ frontend/src/components/DeleteListingForm/index.js
@@ -1,22 +1,42 @@
 import './DeleteListingForm.css';
-import { useDispatch } from 'react-redux';
-import { deleteListing } from '../../store/listings'
+import { useDispatch, useSelector } from 'react-redux';
+import listingsReducer, { deleteListing, getListings } from '../../store/listings'
 import { useHistory } from 'react-router-dom'
+import { useState, useEffect } from 'react';
 
 const DeleteListingForm = ({ listing, visible, setVisible, user }) => {
-  const history = useHistory();
+  const [deleted, setDeleted] = useState(false);
+  // const [showConfirmation, setShowConfirmation] = useState(false)
   const dispatch = useDispatch();
 
-  const handleDelete = () => {
-    dispatch(deleteListing(listing.id))
-    history.push('')
-  }
+  const userListings = useSelector(state => state.listings.listings.filter(listing => {
+    return listing.userId === user.id;
+  }));
+
+  const history = useHistory();
+
+  useEffect(() => {
+    dispatch(getListings())
+  }, [deleted]);
+
+  // console.log('userListings', listings)
+  const handleDelete = async (id) => {
+    // console.log('id:: ', id)
+    try {
+      await dispatch(deleteListing(id))
+    } catch (error) {
+      //
+    }
+    setDeleted(true);
+    setVisible(false);
+    history.push(`/users/${user.id}/listings`)
+  };
 
   return (
     <div>
       <h3>Are you sure that you want to delete this listing?</h3>
       <button onClick={() => setVisible(false)}>Cancel</button>
-      <button onClick={handleDelete}>Yes, Delete</button>
+      <button onClick={() => handleDelete(listing.id)}>Yes, Delete</button>
     </div>
   )
 }
