8a44af8319064c9ab9a4343daff03b47f22d4983
Walker Adkins <walkeradkins@gmail.com>
2022-06-01 20:26:22 -0500

    some styling on the home page

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -5,14 +5,24 @@ import ProfileButton from '../ProfileButton';
 import LoginFormModal from '../LoginFormModal';
 import SignupFormModal from '../SignupFormModal';
 import './Navigation.css';
+import logo from '../images/wherebnb.png'
 
-function Navigation({ isLoaded }){
+function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
 
   let sessionLinks;
   if (sessionUser) {
     sessionLinks = (
-      <ProfileButton user={sessionUser} />
+      <>
+        <div className='navbar__search'>
+          <input className='navbar__search-input' />
+          <span class="material-symbols-outlined navbar__search-icon">search</span>
+        </div>
+        <div className='navbar__user'>
+          <NavLink to='/listings' className='navbar__host'>Become a Host</NavLink>
+          <ProfileButton user={sessionUser} />
+        </div>
+      </>
     );
   } else {
     sessionLinks = (
@@ -24,12 +34,15 @@ function Navigation({ isLoaded }){
   }
 
   return (
-    <ul>
-      <li>
-        <NavLink exact to="/">Home</NavLink>
-        {isLoaded && sessionLinks}
-      </li>
-    </ul>
+    <nav className='navbar'>
+      <header className='navbar__header'>
+        <NavLink className='navbar__logo' exact to="/">
+          <img src={logo} className='navbar__logo-image' />
+          wherebnb
+        </NavLink>
+      </header>
+      {isLoaded && sessionLinks}
+    </nav>
   );
 }
 
